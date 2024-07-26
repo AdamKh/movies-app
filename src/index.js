dc9e04ff34008d17a2fa13a03999a74c144be0da
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-07-26 14:57:34 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const getResource = async (url) => {
+  const res = await fetch(url).json
+  const body = await res.json
+  return body
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+getResource('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1').then((body) => {
+  console.log(body)
+})
