49cd36df7eec828eb5d546d30f9c84df82399424
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-07-31 20:53:40 +0300

    added hint to enable VPN

--- src/components/error/error-handler/error-handler.js
+++ src/components/error/error-handler/error-handler.js
@@ -2,5 +2,13 @@ import '../error.css'
 import { Alert } from 'antd'
 
 export default function ErrorHandler({ errorMessage, errorDescription }) {
-  return <Alert className="error" message={errorMessage} description={errorDescription} type="error" showIcon />
+  return (
+    <Alert
+      className="error"
+      message={errorMessage}
+      description={`${errorDescription}. Maybe you need to enable VPN ZenMate`}
+      type="error"
+      showIcon
+    />
+  )
 }
